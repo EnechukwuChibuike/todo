d6428f0bd610de3f3e1d4615cd4e0fae13fa634d
chibuike <enechukwuchibuike0@gmail.com>
2024-12-07 16:18:11 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,105 @@
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/service-worker.js")
+      .then((registration) => {
+        console.log(
+          "Service Worker registered with scope:",
+          registration.scope
+        );
+      })
+      .catch((err) => {
+        console.error("Service Worker registration failed:", err);
+      });
+  });
+}
+
+const form = document.querySelector("#form");
+const input = document.querySelector("#input");
+const ol = document.querySelector("#ol");
+const undo = document.querySelector(".undo");
+const restore = document.querySelector(".restore");
+
+let todo = getFromLocalStorage();
+let deletedTodo = [];
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  if (input.value === "") {
+    input.style.border = "1px solid red";
+    return;
+  }
+
+  let list = document.createElement("p");
+  let delTodo = document.createElement("button");
+
+  let div = document.createElement("div");
+
+  ol.appendChild(div);
+
+  div.appendChild(list);
+  div.appendChild(delTodo);
+
+  let task = input.value;
+
+  list.innerText = task;
+  delTodo.innerText = "Delete";
+
+  todo.push(task);
+
+  console.log(todo);
+
+  localStorage.setItem("todo", JSON.stringify(todo));
+
+  //   delete Todo
+  delTodo.onclick = () => {
+    ol.removeChild(div);
+    deletedTodo.push(div);
+    console.log(deletedTodo);
+  };
+
+  //   undo deleted Todo
+  undo.onclick = () => {
+    ol.appendChild(deletedTodo[deletedTodo.length - 1]);
+    deletedTodo.pop();
+  };
+
+  //   restore all deleted Todo
+  restore.onclick = () => {
+    deletedTodo.map((todo) => {
+      ol.appendChild(todo);
+    });
+  };
+
+  input.value = "";
+});
+
+function displayFromLocalStorage() {
+  let getTodo = getFromLocalStorage();
+
+  getTodo.forEach((todo) => {
+    let list = document.createElement("p");
+    let delTodo = document.createElement("button");
+
+    let div = document.createElement("div");
+    div.appendChild(list);
+    div.appendChild(delTodo);
+
+    list.innerText = todo;
+    delTodo.innerText = "Delete";
+
+    ol.appendChild(div);
+  });
+}
+
+displayFromLocalStorage();
+
+function getFromLocalStorage() {
+  return JSON.parse(localStorage.getItem("todo"));
+}
+
+// getFromLocalStorage();
+
+input.addEventListener("focus", () => {
+  input.style.border = "none";
+});
