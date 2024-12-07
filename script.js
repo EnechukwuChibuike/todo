7b6ef33b2aaec9f5726bd7701adc03118c02811c
chibuike <enechukwuchibuike0@gmail.com>
2024-12-07 18:16:48 +0100

    first commit

--- script.js
+++ script.js
@@ -14,6 +14,34 @@ if ("serviceWorker" in navigator) {
   });
 }
 
+let deferredPrompt;
+
+window.addEventListener("beforeinstallprompt", (event) => {
+  // Prevent the mini-infobar from appearing on mobile
+  event.preventDefault();
+  // Stash the event so it can be triggered later
+  deferredPrompt = event;
+
+  // Show your "Install" button
+  const installButton = document.getElementById("install-button");
+  installButton.style.display = "block";
+
+  installButton.addEventListener("click", () => {
+    // Show the install prompt
+    deferredPrompt.prompt();
+
+    // Wait for the user to respond to the prompt
+    deferredPrompt.userChoice.then((choiceResult) => {
+      if (choiceResult.outcome === "accepted") {
+        console.log("User accepted the install prompt");
+      } else {
+        console.log("User dismissed the install prompt");
+      }
+      deferredPrompt = null; // Clear the deferred prompt
+    });
+  });
+});
+
 const form = document.querySelector("#form");
 const input = document.querySelector("#input");
 const ol = document.querySelector("#ol");
